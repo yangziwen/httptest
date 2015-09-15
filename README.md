93bb1aa0ffee54c0c7c6d4fbacc9703bf7b7d4bf
zyang <yzwcroco@gmail.com>
2015-09-15 19:20:23 +0800

    修改数据库文件的生成机制

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Http接口测试工
+# Http接口测试工具
 
 ### 介绍
 本工具用于测试http接口的返回结果，可自定义请求的参数、上传文件路径、header和cookie的值，并支持html，json，jsonp等响应类型。
@@ -9,21 +9,13 @@
 
 &ensp;&ensp;`git clone https://github.com/yangziwen/httptest.git`
 
-&ensp;&ensp;**2.初始化数据库**
-
-&ensp;&ensp;`mvn compile exec:exec -Pinit-db`
-
-&ensp;&ensp;生成名为db的目录，其中包含名为httptest.db的数据库文件
-
-&ensp;&ensp;**3.生成war包**
+&ensp;&ensp;**2.生成war包**
 
 &ensp;&ensp;`mvn clean package -Pcompress -Pstandalone`
 
 &ensp;&ensp;生成httptest-standalone.war
 
 &ensp;&ensp;**3.运行工程**
-
-&ensp;&ensp;将前面生成的db目录与httptest-standalone.war文件放置在同一路径下，并执行以下命令
 
 &ensp;&ensp;`java -Dport=8989 -Dcontextpath=/ -jar httptest-standalone.war`
 
--- src/main/java/net/yangziwen/httptest/util/DBInitiator.java
+++ src/main/java/net/yangziwen/httptest/util/DBInitiator.java
@@ -3,35 +3,82 @@ package net.yangziwen.httptest.util;
 import java.io.File;
 import java.io.IOException;
 
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.sql.DataSource;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.SystemUtils;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.stereotype.Component;
 
-public class DBInitiator {
+/**
+ * 如果不存在sqlite数据库文件，则自动生成并初始化
+ * 也可以调用 mvn compile exec:exec -Pinit-db手动生成数据库文件
+ */
+@Component
+public class DBInitiator implements ApplicationListener<ContextRefreshedEvent>, ServletContextListener {
 	
 	private static final File DB_FOLDER = new File(FilenameUtils.concat(SystemUtils.USER_DIR, "db"));
 	
-	private DBInitiator() {}
-
-	@SuppressWarnings("resource")
-	public static final void initDatabase() throws IOException {
-		if(!DB_FOLDER.exists()) {
-			DB_FOLDER.mkdirs();
+	public static final File DB_FILE = new File(FilenameUtils.concat(DB_FOLDER.getAbsolutePath(), "httptest.db"));
+	
+	private static boolean needToInitDb = false;
+	
+	public static boolean createDbFileIfNotExist() {
+		if(DB_FILE.exists()) {
+			return false;
 		}
+		try {
+			FileUtils.touch(DB_FILE);
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public static final void initDatabase(DataSource dataSource) {
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+		populator.addScript(new ClassPathResource("sql/sqlite/schema.sql"));
+		DatabasePopulatorUtils.execute(populator, dataSource);
+	}
+	
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		needToInitDb = createDbFileIfNotExist();
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {}
+	
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		if(event.getApplicationContext().getParent() != null) {
+			return;
+		}
+		doAfterRootApplicationContextInitialized(event);
+	}
+	
+	public void doAfterRootApplicationContextInitialized(ContextRefreshedEvent event) {
+		if(needToInitDb) {
+			DataSource dataSource = event.getApplicationContext().getBean(DataSource.class);
+			DBInitiator.initDatabase(dataSource);
+			needToInitDb = false;
+		}
+	}
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws IOException {
+		createDbFileIfNotExist();
 		Profiles.setProfileAsSystemProperty(Profiles.DEVELOPMENT_INIT);
 		new ClassPathXmlApplicationContext("spring/applicationContext.xml");
 	}
-	
-	public static final boolean existsDb() {
-		if(!DB_FOLDER.exists()) {
-			return false;
-		}
-		File dbFile = new File(FilenameUtils.concat(DB_FOLDER.getAbsolutePath(), "httptest.db"));
-		return dbFile.exists();
-	}
-	
-	public static void main(String[] args) throws IOException {
-		initDatabase();
-	}
-	
+
 }
